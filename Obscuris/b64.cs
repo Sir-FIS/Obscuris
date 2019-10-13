8dd86d9bdb48ab0902a6814720c99188919252cc
unknown <marwan.elgendi@yahoo.com>
2019-10-13 22:47:43 +0100

    first commit

---
+++ Obscuris/b64.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Obscuris
+{
+	public	class b64
+	{
+		public static string Base64Encode(string filename)
+		{
+			string plainText = System.IO.File.ReadAllText(filename);
+			var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
+			return System.Convert.ToBase64String(plainTextBytes);
+		}
+		public static string Base64Decode(string filename)
+		{
+			string base64EncodedData = System.IO.File.ReadAllText(filename);
+			var base64EncodedBytes = System.Convert.FromBase64String(base64EncodedData);
+			return System.Text.Encoding.UTF8.GetString(base64EncodedBytes);
+		}
+	}
+}
---
+++ Obscuris/main.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CommandLine;
+using System.Text;
+using System.Threading.Tasks;
+using static Obscuris.b64;
+using static Obscuris.aes;
+
+
+namespace Obscuris
+{
+	class crypto
+	{
+
+		class Options
+		{
+			[Option('i', "input-file", Required = true,
+			HelpText = "Input file to be processed.")]
+			public string InputFile { get; set; }
+
+			[Option('e', "obscuring method", Default= true,
+			  HelpText = "how the file will be processed")]
+			public string obscure { get; set; }
+			
+		}
+
+		static int Main(string[] args)
+		{
+			if (args.Length == 0)
+			{
+				System.Console.WriteLine("Please provide file to encrypt.");
+				return 1;
+			}
+			else
+			{
+				var opts = new Options();
+
+				Parser.Default.ParseArguments<Options>(args).WithParsed(parsed => opts = parsed);
+				string filename = opts.InputFile;
+				string obscure = opts.obscure;
+				string result = input(obscure, filename);
+				System.Console.WriteLine(result);
+				return 0;
+			}
+		}
+
+		public static string input(string obscure, string filename)
+		{
+			switch (obscure)
+			{
+				case "b64":
+
+				string	val = Base64Encode(filename); 
+					return val;
+
+				case "d64":
+				 val = Base64Decode(filename);
+					return val;
+
+					
+				case "encrypt":
+					var instance = new aes();
+					Console.Write("Enter a string - ");
+					string password = Console.ReadLine();
+					string encryptedText = instance.Encrypt(password, filename);
+					return encryptedText;
+
+				case "decrypt":
+					instance = new aes();
+					Console.Write("Enter password - ");
+					password = Console.ReadLine();
+					string decryptedText = instance.Decrypt(password, filename);
+					return decryptedText;
+
+				default:
+					System.Console.WriteLine("invalid obsuring argument provided");
+					val = null;
+					return val;
+	
+
+			}
+		}
+
+	}
+	}
+
